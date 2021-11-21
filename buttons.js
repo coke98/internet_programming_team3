88a88825ec3da38c0cb5b9433335bf4f08f7f05d
coke98 <87576669+coke98@users.noreply.github.com>
2021-11-21 23:46:39 +0900

    Add files via upload
    마커 이미지 삽입

---
+++ buttons.js
@@ -0,0 +1,48 @@
+// 사이드바 on, off
+function sidebar_wrap_on() {
+    var sidebar_wrap = document.getElementById("sidebar_wrap");
+    sidebar_wrap.style.display = "block";
+}
+function sidebar_wrap_off() {
+    var sidebar_wrap = document.getElementById("sidebar_wrap");
+    sidebar_wrap.style.display = "none";
+}
+
+// 지도 확대, 축소
+function zoomIn() {
+    map.setLevel(map.getLevel() - 1);
+}
+function zoomOut() {
+    map.setLevel(map.getLevel() + 1);
+}
+
+// 현재 위치 불러오기
+function current_location() {
+    // 기존 현재위치 마커 지우기
+    if (current_location_marker != null) {
+        map.removeOverlayMapObject(current_location_marker);
+    }
+    // 현재 위치 불러오기
+    navigator.geolocation.getCurrentPosition(function (position) {
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
+        var latlng = new kakao.maps.LatLng(lat, lng);
+        var marker = new kakao.maps.Marker({
+            position: latlng,
+        });
+        marker.setMap(map);
+        current_location_marker = marker;
+        map.setCenter(latlng);
+    });
+}
+
+// 사이렌 소리 toggle
+function siren() {
+    var siren = document.getElementById("siren");
+    if (siren.paused) {
+        siren.currentTime = 0;
+        siren.play();
+    } else {
+        siren.pause();
+    }
+}
