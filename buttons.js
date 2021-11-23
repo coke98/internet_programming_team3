a90e095d65cf8577cb43772d67e083d58e1c44c0
coke98 <87576669+coke98@users.noreply.github.com>
2021-11-23 22:51:19 +0900

    Add files via upload

--- buttons.js
+++ buttons.js
@@ -61,7 +61,7 @@ function siren() {
 
 // 사이드바 open
 function openSidebar() {
-    document.getElementById("sidebar").style.width = "140px";
+    document.getElementById("sidebar").style.width = "135px";
     var subSidebar = document.getElementsByClassName("subSidebar");
     for (var i = 0; i < subSidebar.length; i++) {
         document.getElementsByClassName("subSidebar")[i].style.width = "0px";
